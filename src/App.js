3d3b43778566f191ee93ad783656a31f6c933116
Lakshani Erandika <lakshaniudawela@gmail.com>
2024-04-23 21:11:12 +0530

    App code Change

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
+import { Navbar, Footer } from './Components/Layout';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Courses from './Components/Courses';
+import Student from './Components/Students'
+import Enrollment from './Components/Enrollments'
+import Home from './Components/Home';
+import LandingPage from './Components/LandingPage';
 
 function App() {
   return (
     <div className="App">
-      
+      {/* <LandingPage/> */}
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+      <Route path='/Home' element={<Home/>}></Route>
+      <Route path='/Courses' element={<Courses/>}></Route>
+      <Route path='/Student' element={<Student/>}></Route>
+      <Route path='/Enrollment' element={<Enrollment/>}></Route>
+    </Routes>
+    <Footer/>
+    </BrowserRouter>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,29 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Navbar, Footer } from './Components/Layout';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Courses from './Components/Courses';
-import Student from './Components/Students'
-import Enrollment from './Components/Enrollments'
-import Home from './Components/Home';
-import LandingPage from './Components/LandingPage';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-  {/* <LandingPage/> */}
-  <BrowserRouter>
-  <Navbar/>
-  <Routes>
-    <Route path='/Home' element={<Home/>}></Route>
-    <Route path='/Courses' element={<Courses/>}></Route>
-    <Route path='/Student' element={<Student/>}></Route>
-    <Route path='/Enrollment' element={<Enrollment/>}></Route>
-  </Routes>
-  <Footer/>
-  </BrowserRouter>
- 
+  <App/>
   </>
 );
 
